757db2aa8fdd3238c6bfb0df994e9a25edb644e5
guyot <WangGY116411@yimidida.com>
2020-12-21 08:51:03 +0800

    isUnivalTree

--- src/java/com/guyot/study/algo/leetcode/editor/cn/tree/Solution.java
+++ src/java/com/guyot/study/algo/leetcode/editor/cn/tree/Solution.java
@@ -122,4 +122,17 @@ public class Solution {
         }
         return recursive(left.left, right.right) && recursive(left.right, right.left);
     }
+
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.left != null && root.val != root.left.val){
+            return false;
+        }
+        if (root.right != null && root.val != root.right.val) {
+            return false;
+        }
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+    }
 }
